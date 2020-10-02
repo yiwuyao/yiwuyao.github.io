36f1aaa2a7787ea0e41c77bab5eeec6f4787cf69
Windranger115 <59741293+Windranger115@users.noreply.github.com>
2020-10-01 23:02:12 -0400

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-75px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// document ready
+// (function ($) {
+
+//     // scroll functions
+//     $(window).scroll(function(e) {
+    
+//         // add/remove class to navbar when scrolling to hide/show
+//         $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
+    
+//     });
+    
+// })(jQuery); 
+
+// document ready
+// (function ($) {
+
+//     // scroll functions
+//     $(window).scroll(function(e) {
+    
+//         // add/remove class to navbar when scrolling to hide/show
+//         var scroll = $(window).scrollTop();
+//         if (scroll >= 150) {
+//             $('.navbar').addClass("navbar-hide");
+//         } else {
+//             $('.navbar').removeClass("navbar-hide");
+//         }
+    
+//     });
